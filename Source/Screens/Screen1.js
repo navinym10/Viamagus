39e765df08c17056dbcd3d28c36df22b777ad46f
navinym10 <navinprasanthr@gmail.com>
2023-10-18 19:34:25 +0530

    fonts setup

--- Source/Screens/Screen1.js
+++ Source/Screens/Screen1.js
@@ -1,11 +1,13 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { Button } from 'react-native'
+import { Fonts } from '../Fonts/Fonts'
 
 const Screen1 = ({ navigation }) => {
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Screen1</Text>
+            <Text style={{ fontFamily: Fonts.montserrat_Regular }}>Screen1</Text>
+            <Text style={{}}>Screen1</Text>
             <Button title='Click' onPress={() => navigation.navigate('Screen2')} />
         </View>
     )
